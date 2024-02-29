e06d8b799d3fb1fc0a875dbd50be8fc07b6b06f4
Mujahid <eng.mdmuzahidulislam@gmail.com>
2024-02-29 12:22:54 +0600

    items found or not found message in text

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 // all common DOM selector collections
 const gadgetsCardContainer = document.getElementById("gadgetsCardContainer");
 const searchField = document.getElementById("searchField");
+const foundItems = document.getElementById("foundItems");
 
 // fetch and load data from API
 const loadData = async (search) => {
     const load = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
     const data = await load.json()
+
+    // clear the found items initially
+    foundItems.innerText =``
+    if (data.data.length) {
+        const itemsFound = document.createElement("h2")
+        itemsFound.classList.add("text-xl", "text-green-600")
+        itemsFound.innerText = `${data.data.length} items found`
+        foundItems.appendChild(itemsFound)
         createGadgetCard(data.data);
+    }
+    else {
+        const notFound = document.createElement("h2")
+        notFound.classList.add("text-xl", "text-red-600")
+        notFound.innerText = "No items found. Please search again"
+        foundItems.appendChild(notFound)
+    }
 }
 
 
@@ -14,7 +30,7 @@ const loadData = async (search) => {
 const createGadgetCard = (data) => {
     data.forEach(gadget => {
         const gadgetsCard = document.createElement("div");
-        gadgetsCard.classList.add("px-6", "py-4", "bg-red-100" ,"rounded-xl")
+        gadgetsCard.classList.add("px-6", "py-4", "bg-red-100", "rounded-xl")
         gadgetsCard.innerHTML = `
         <img src=${gadget.image} alt="" class="w-2/3 mx-auto rounded-2xl">
         <h2 class="my-4 text-4xl font-bold text-amber-800">${gadget.brand}</h2>
@@ -28,12 +44,12 @@ const createGadgetCard = (data) => {
 }
 
 // handle search button
-const searchButton = () =>{
-    if(searchField.value){
+const searchButton = () => {
+    if (searchField.value) {
         gadgetsCardContainer.innerHTML = ''
         loadData(searchField.value);
     }
-    else{
+    else {
         alert("search field is empty")
     }
 }
